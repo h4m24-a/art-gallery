ed46b8661cebfbe138c782f71092035a8882e592
Hamza <102926214+h4m24-a@users.noreply.github.com>
2023-03-27 23:32:24 +0100

    Update viewbasket.php

--- viewbasket.php
+++ viewbasket.php
@@ -19,35 +19,26 @@ while($row = mysqli_fetch_array($result))
 mysqli_close($conn);
 //$orderTotal=0;
 ?>
-<head>
 
-<title> Shopping Cart</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="nav.css">
+	<link rel="stylesheet" type="text/css" href="image_test_styles.css">
+    <title>Shopping Cart</title>
+  </head>
+  <body> 
 
 
+<body style="background-color:#ECF0F1;"></body>
 <img style="float:left" src="image/ArtGalleryLogo.png"" alt="ArtGallery" >
-<link rel="stylesheet" type="text/css" href="style.css">
-<link rel="stylesheet" type="text/css" href="nav.css">
-<link rel="stylesheet" type="text/css" href="image_test_styles.css">
 
-<body style="background-color:#ECF0F1;">
- 
-   </body>
-
-
-
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-<body>
 
 <div class="header">
-
-<body style="background-color:#ECF0F1;">
- </body>
- 
   <div class="header-right">
     <a class="active" href="index.html">Home</a>
     <a href="products.php">Products</a>
@@ -57,22 +48,9 @@ mysqli_close($conn);
 	
 	<a href="loghome.php"> <img src="image/profile.png" width="30" height="30" style="float:right"></a>
 	<a href="viewbasket.php"> <img src="image/cart.png" width="30" height="30" style="float:right"></a>
-	
-
-	
   </div>
 </div>
-</body>
-</html>
 
-
-
-
-<html>
-<head>
-<title> Shopping Basket</title>
-</head>
-<body>
 
 <h2>Shopping Basket</h2>
 
@@ -118,18 +96,12 @@ mysqli_close($conn);
 <?php echo "Total Cost : £ $orderTotal";?> <br>
 			<input type="submit" name = "submit" value="Checkout"/>
 		</form>
-
-
 <?php
-
 ?>
-</body>
-</html>	
 
-
+																 
 
 <footer>
-
 
 <a  href="https://www.instagram.com/">
 <img src="image/instagram.png"  width="25" height="25" style="float:right; margin:15px">
@@ -143,6 +115,8 @@ mysqli_close($conn);
 <a  href="https://www.facebook.com/">
  <img src="image/facebook.png"  width="25" height="25" style="float: right; margin:15px">
 </a>
-
-
+																						
 </footer>
+
+</body>
+</html>
