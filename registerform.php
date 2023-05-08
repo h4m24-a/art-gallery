5f5b4a0be3f921a56810f7e2d07c92bfdeb70c23
Hamza <102926214+h4m24-a@users.noreply.github.com>
2023-05-08 19:40:20 +0100

    Update registerform.php

--- registerform.php
+++ registerform.php
@@ -1,34 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   	<link rel="stylesheet" type="text/css" href="register.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="stylesheet" type="text/css" href="nav.css">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-	<title>Register Form</title>
-  </head>
-  <body> 
-	  
- <body style="background-color:#ECF0F1;"></body>
- <img style="float:left" src="image/ArtGalleryLogo.png"" alt="ArtGallery" >
-
-<div class="header"> 
-  <div class="header-right">
-    <a class="active" href="index.html">Home</a>
-    <a href="viewgallery.php">Products</a>
-	<a href="sidebyside2.php">View Gallery</a>
-	
-	<a href="log_in_form.php">Log In</a> 
-	
-	<a href="loghome.php"> <img src="image/profile.png" width="30" height="30" style="float:right"></a>
-	<a href="viewbasket.php"> <img src="image/cart.png" width="30" height="30" style="float:right"></a>
-	
-  </div>
-</div>
-
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="register.css">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="nav.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+		<title>Register Form</title>
+	</head>
+	<body>
+		<body style="background-color:#ECF0F1;"></body>
+		<img style="float:left" src="image/ArtGalleryLogo.png"" alt=" ArtGallery">
+		<div class="header">
+			<div class="header-right">
+				<a class="active" href="index.html">Home</a>
+				<a href="viewgallery.php">Products</a>
+				<a href="sidebyside2.php">View Gallery</a>
+				<a href="log_in_form.php">Log In</a>
+				<a href="loghome.php">
+					<img src="image/profile.png" width="30" height="30" style="float:right">
+				</a>
+				<a href="viewbasket.php">
+					<img src="image/cart.png" width="30" height="30" style="float:right">
+				</a>
+			</div>
+		</div>
 		<div class="register">
 			<h1>Register</h1>
 			<form action="reg.php" method="post" autocomplete="off">
@@ -40,11 +38,8 @@
 					<i class="fas fa-lock"></i>
 				</label>
 				<input type="password" name="password" placeholder="Password" id="password" required>
-				
-				
 				<input type="submit" value="Register">
 			</form>
 		</div>
-													 
-</body>
+	</body>
 </html>
